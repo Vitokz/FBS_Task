8d60cb1c8b32639a061fcd74679f3e36768ebad1
Vito_ <vit.koz@faem.me>
2021-09-10 22:37:16 +0300

    add max "to" param in fibonacci

--- gRPC/rpc/fibonacci.go
+++ gRPC/rpc/fibonacci.go
@@ -10,45 +10,47 @@ import (
 func (g *GRPCServer) CalculateFibonacci(ctx context.Context, request *FibRequest) (*FibResponse, error) {
 	from := request.GetFrom()
 	if from == "" {
-		err:=errors.New("Query param \"to\" is empty")
+		err := errors.New("Query param \"to\" is empty")
 		g.Handler.Log.Error(err)
-		return nil,err
+		return nil, err
 	}
-	fromInt, err:=strconv.Atoi(from)
+	fromInt, err := strconv.Atoi(from)
 	if err != nil {
 		return nil, errors.New("\"from\" param is not number")
-	}else if fromInt < 0 {
+	} else if fromInt < 0 {
 		return nil, errors.New("\"from\" param is minus")
 	}
 
 	to := request.GetTo()
-	if to == ""{
-		err:=errors.New("Query param \"to\" is empty")
+	if to == "" {
+		err := errors.New("Query param \"to\" is empty")
 		g.Handler.Log.Error(err)
-		return &FibResponse{},err
+		return &FibResponse{}, err
 	}
 	toInt, err := strconv.Atoi(to)
 	if err != nil {
 		return nil, errors.New("\"to\" param is not number")
-	}else if toInt < 0 {
+	} else if toInt < 0 {
 		return nil, errors.New("\"to\" param is minus")
-	}else if toInt < fromInt {
+	} else if toInt < fromInt {
 		return nil, errors.New("\"to\" param is less than \"from\"")
+	} else if toInt > 92 {
+		return nil, errors.New("\"to\" param must be less than or equal to 92")
 	}
 
 	g.Handler.Log.WithFields(logrus.Fields{
-		"event" : "Calculate Fibonacci",
-		"from" : from,
-		"to" : to,
+		"event": "Calculate Fibonacci",
+		"from":  from,
+		"to":    to,
 	}).Info()
 
-	resp,err := g.Handler.Fibonacci(fromInt,toInt)
+	resp, err := g.Handler.Fibonacci(fromInt, toInt)
 	if err != nil {
 		g.Handler.Log.Error(err)
-		return &FibResponse{},err
+		return &FibResponse{}, err
 	}
 
 	return &FibResponse{
 		Numbers: resp.Numbers,
-	},nil
+	}, nil
 }
